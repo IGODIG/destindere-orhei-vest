0b81e81d1e572a37631b2d249f84bcad607c6930
Igor Digori <105446869+IGODIG@users.noreply.github.com>
2026-09-23 23:10:27 +0300

    Restore production repository before test changes

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const DEFAULT_CONFIG = {
     heroTitle: "DESTINDERE",
     heroSubtitle: "Iată ce bine și ce plăcut este ca frații să locuiască împreună în unitate!",
     heroVerse: "Iată ce bine și ce plăcut este ca frații să locuiască împreună în unitate!",
-    heroImage: "./assets/images/image.png",
+    heroImage: "./assets/images/hero/hero.jpg",
     footer: "© 2026 Destindere de Toamnă • Congregația Orhei-Vest"
   },
 
